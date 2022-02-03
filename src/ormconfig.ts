b59bc8dd7cb3bdd961d80deac0c11baa3c7d80d7
Va1erii <valerapopovsakha@gmail.com>
2022-02-03 13:08:59 +0300

    Added migration

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,7 +8,11 @@ import { ConnectionOptions } from "typeorm";
      password: 'Admin123',
      database: 'mediumclone',
      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-     synchronize: true
+     synchronize: false,
+     migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+     cli: {
+         migrationsDir: 'src/migrations'
+     }
  };
 
  export default config;
